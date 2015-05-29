a96a410b62e8334ad510f9565228b8034d3da43c
Gautam Gupta <blerb795@gmail.com>
2015-05-29 00:16:46 -0700

    set up some more routes and structure

--- dorm.py
+++ dorm.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort, url_for, request
 app = Flask(__name__)
+
+dorms = ["rieberterrace", "riebervista", "rieberhall", "hedrickhall",
+         "hedricksummit", "sproulhall", "sproulcove", "sproullanding",
+         "hitchsuites", "dykstrahall", "deneve"]
 
 @app.route('/')
 def main_page():
     return render_template('index.html')
 
+
+@app.route('/<dorm>/')
+def dorm_page(dorm):
+    if dorm not in dorms:
+        abort(404)
+    #data = get_data_from_database(dorm)
+    #return render_template('dorm.html', dorm=dorm, data=data)
+
+
+
+
+@app.errorhandler(404)
+def error(e):
+    #return render_template('404.html'), 404
+    return "404 page not found" # for now
 
 if __name__ == '__main__':
     app.run()
